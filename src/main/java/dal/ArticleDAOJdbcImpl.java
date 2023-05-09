0137ff9f9cf959b46ded0ae48b2092db14f30e83
Edouard Govillot <edouard.govillot@gmail.com>
2023-05-09 14:20:52 +0200

    create auctions (not tested) / upload images management (not tested)

--- src/main/java/dal/ArticleDAOJdbcImpl.java
+++ src/main/java/dal/ArticleDAOJdbcImpl.java
@@ -4,17 +4,14 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.Instant;
-import java.time.LocalDate;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import bo.Category;
 import bo.Item;
-import bo.User;
+import bo.PickupPoint;
 
 public class ArticleDAOJdbcImpl implements ArticleDAO{
 
@@ -24,7 +21,8 @@ public class ArticleDAOJdbcImpl implements ArticleDAO{
 													  "etat_vente, image FROM ARTICLES_VENDUS A " + 
 													  "JOIN CATEGORIES ON CATEGORIES.no_categorie = A.no_categorie " + 
 													  "JOIN UTILISATEURS ON UTILISATEURS.no_utilisateur = A.no_utilisateur ";
-	
+	private static final String INSERT_ARTICLE = "INSERT INTO ARTICLES_VENDUS";
+	private static final String CREATE_PICKUP_POINT = "INSERT INTO RETRAIT VALUES (?, ?, ?, ?)";
 	
 	@Override
 	public List<Category> listAllCategories() {
@@ -56,7 +54,7 @@ public class ArticleDAOJdbcImpl implements ArticleDAO{
 		List<Item> result = new ArrayList<>();
 		try (Connection cnx = ConnectionProvider.getConnection();) {
 			
-			PreparedStatement ps = cnx.prepareStatement(SELECT_ALL_ARTICLES, PreparedStatement.RETURN_GENERATED_KEYS);
+			PreparedStatement ps = cnx.prepareStatement(SELECT_ALL_ARTICLES);
 			
 			ResultSet rs = ps.executeQuery();
 			
@@ -65,26 +63,16 @@ public class ArticleDAOJdbcImpl implements ArticleDAO{
 				newItem.setNo_article(rs.getInt("no_article"));
 				newItem.setName_article(rs.getString("nom_article"));
 				newItem.setDescr_article(rs.getString("description"));
-				
 				LocalDateTime localDateTime = rs.getTimestamp("date_debut_enchere").toLocalDateTime();
 				newItem.setStart_auction(localDateTime);
-				
 				LocalDateTime localDateTime2 = rs.getTimestamp("date_fin_enchere").toLocalDateTime();
 				newItem.setEnd_auction(localDateTime2);
-				
 				newItem.setInitial_price(rs.getInt("prix_initial"));
 				newItem.setSell_price(rs.getInt("prix_vente"));
-				
-				User user = new User();
-				user.setNo_user(rs.getInt("no_utilisateur"));
-				user.setPseudo(rs.getString("USER_PSEUDO"));
-				newItem.setUser(user);
-				
-				Category cat = new Category();
-				cat.setNo_categorie(rs.getInt("no_categorie"));
-				cat.setLibelle(rs.getString("CAT_LIB"));
-				newItem.setCategory(cat);
-				
+				newItem.setNo_user(rs.getInt("no_utilisateur"));
+				newItem.setUser_Pseudo(rs.getString("USER_PSEUDO"));
+				newItem.setNo_category(rs.getInt("no_categorie"));
+				newItem.setCategory_Lib(rs.getString("CAT_LIB"));
 				newItem.setSell_status(rs.getString("etat_vente"));
 				newItem.setImage_article(rs.getString("image"));
 				
@@ -96,6 +84,39 @@ public class ArticleDAOJdbcImpl implements ArticleDAO{
 			e.printStackTrace();
 		}
 		return result;
+	}
+
+
+	@Override
+	public void insertItem(Item item, PickupPoint pickupPoint) {
+		try (Connection cnx = ConnectionProvider.getConnection();) {
+			
+			PreparedStatement ps = cnx.prepareStatement(INSERT_ARTICLE, PreparedStatement.RETURN_GENERATED_KEYS);
+			
+			ps.setString(1, item.getName_article());
+			ps.setString(2, item.getDescr_article());
+			ps.setTimestamp(3, Timestamp.valueOf(item.getStart_auction()));
+			ps.setTimestamp(4, Timestamp.valueOf(item.getEnd_auction()));
+			ps.setInt(5, item.getInitial_price());
+			ps.setNull(6, java.sql.Types.INTEGER);
+			ps.setInt(7, item.getNo_user());
+			ps.setInt(8, item.getNo_category());
+			ps.setString(9, item.getSell_status());
+			ps.setString(10, item.getImage_article());
+			
+			ps.executeUpdate();
+			
+			ResultSet rs = ps.getGeneratedKeys();
+			PreparedStatement ps2_pickUp = cnx.prepareStatement(CREATE_PICKUP_POINT);
+			ps2_pickUp.setInt(1, rs.getInt(1));
+			ps2_pickUp.setString(2, pickupPoint.getStreetAddress());
+			ps2_pickUp.setString(3, pickupPoint.getPostalCode());
+			ps2_pickUp.setString(4, pickupPoint.getCityAddress());
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 	
--- src/main/webapp/WEB-INF/jsp/create-auction.jsp
+++ src/main/webapp/WEB-INF/jsp/create-auction.jsp
@@ -9,7 +9,7 @@
 	
 	<img id="uploadPreview" style="width: 100px; height: 100px" />
 	
-	<form action="createAuction" method="POST">
+	<form action="createAuction" method="POST" enctype="multipart/form-data">
 		
 		<label for="articleName">Article :</label>
 		<input type="text" name="articleName" />
@@ -22,15 +22,35 @@
 				<option value="">Toutes</option>
 				
 				<c:forEach var="cat" items="${categories}">
-					<option value="${cat }">${cat.getLibelle() }</option>
+					<option value="${cat.no_categorie }">${cat.getLibelle() }</option>
 				</c:forEach>
 			</select>
 		
 		<label for="photo">Photo de l'article</label>
 		<input type="file" id="pictureFile" name="pictureFile" accept="image/png, image/jpeg" onchange="PreviewImage()"/>
 		
-		<button type="submit" name="save">Enregistrer</button>
-		<button type="submit" name="cancel">Annuler</button>
+		<label for="price">Mise à prix :</label>
+		<input type="number" name="price" step=10 />
+		
+		<label for="startAuction">Début de l'enchère :</label>
+		<input type="datetime-local" name="startAuction" value="${ currentDate }"/>
+		
+		<label for="endAuction">Fin de l'enchère :</label>
+		<input type="datetime-local" name="endAuction" value="${ endAuctionDate }"/>
+		
+		<p>Retrait</p>
+		<label for="streetAddress">Rue :</label>
+		<input type="text" name="streetAddress" value="${user.cityAddress }" />
+		
+		<label for="postalCode">Code postal :</label>
+		<input type="text" name="postalCode" value="${ user.postalCodeAddress }" />
+		
+		<label for="cityAddress">Ville :</label>
+		<input type="text" name="cityAddress" value="${ user.cityAddress }" />
+		
+		
+		<button type="submit" value="save" name="action" >Enregistrer</button>
+		<button type="submit" value="cancel" name="action" >Annuler</button>
 		
 	</form>
 	
